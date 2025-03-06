a7b33b637d7534af853a612275342d6ac52adf58
TheSavage <TheSavage@users.noreply.github.com>
2025-03-05 17:10:32 -0800

    Add files via upload

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -27,7 +27,7 @@ function compose_email() {
       body: JSON.stringify({
           recipients: '',
           subject: 'Meeting timeX',
-          body: 'How about we meet tomorrow at 3pm?'
+          body: 'How about we meet tomorrow at 7pm?'
       })
     })
     .then(response => response.json())
